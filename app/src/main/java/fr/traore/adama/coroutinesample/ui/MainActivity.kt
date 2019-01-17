dcfe9a37c931e98cb45299b59bf3a9bc280389c9
Adama <a.traore.pro@gmail.com>
2019-01-17 14:40:06 +0100

    observable

--- app/src/main/java/fr/traore/adama/coroutinesample/ui/MainActivity.kt
+++ app/src/main/java/fr/traore/adama/coroutinesample/ui/MainActivity.kt
@@ -1,13 +1,27 @@
 package fr.traore.adama.coroutinesample.ui
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import fr.traore.adama.coroutinesample.R
+import fr.traore.adama.coroutinesample.TmdbViewModel
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val tmdbViewModel = ViewModelProviders.of(this).get(TmdbViewModel::class.java)
+
+        tmdbViewModel.fetchMovies()
+
+        tmdbViewModel.popularMoviesLiveData.observe(this, Observer {
+            Log.d("Observable", "Count "+tmdbViewModel.popularMoviesLiveData.value?.size)
+        })
     }
 }
