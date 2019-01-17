83b1f5bfadf200e64e9df76ef12b573c178d91c9
Adama <a.traore.pro@gmail.com>
2019-01-17 17:18:54 +0100

    base image url

--- app/src/main/java/fr/traore/adama/coroutinesample/Constants.kt
+++ app/src/main/java/fr/traore/adama/coroutinesample/Constants.kt
@@ -4,5 +4,6 @@ public class Constants{
     companion object {
         var tmdbApiKey = BuildConfig.TMDB_API_KEY
         val baseUrl = "https://api.themoviedb.org/3/"
+        val baseImageUrl = "https://image.tmdb.org/t/p/w500/"
     }
 }
